d32946037c1d6f396b63757a864abbfb2d41bdc4
SueWhitcomb <39161955+SueWhitcomb@users.noreply.github.com>
2018-05-20 12:45:48 -0500

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/SortTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+public class SortTests
+{
+
+
+    public SortTests()
+    {
+    }
+    public void ReadFromFile(int[] numbers)
+    {
+        string path = "c:\\users\\sue\\source\\repos\\ConsoleApp5\\ConsoleApp5\\numbers.txt";
+        StreamReader sr = new StreamReader(path);
+
+        for (int index = 0; index < numbers.Length; index++)
+        {
+            numbers[index] = Convert.ToInt32(sr.ReadLine());
+
+        }
+        sr.Close();
+    }
+    public void DisplayArray(int[] numbers)
+    {
+        for (int index = 0; index < numbers.Length; index++)
+        {
+            Console.WriteLine("{0}", numbers[index]);
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/SortTestsTrial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class SortTestsTrial
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = new int[20];
+        SortTests obj = new SortTests();
+
+        obj.ReadFromFile(numbers);
+        obj.DisplayArray(numbers);
+
+        Console.ReadLine();
+    }
+}
